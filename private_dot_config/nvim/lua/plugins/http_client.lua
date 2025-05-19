becdd99a60673349d39c8af8c63a584cd13605f2
ziimir <ziimir9@gmail.com>
2025-05-19 11:04:39 +0400

    up vim config

--- private_dot_config/nvim/lua/plugins/http_client.lua
+++ private_dot_config/nvim/lua/plugins/http_client.lua
@@ -7,5 +7,13 @@ return {
             ["--include"] = "",
         }
         vim.g.vrc_include_response_header = 1
+
+        vim.api.nvim_create_user_command(
+            "SetRestFT",
+            function()
+                vim.cmd("set ft=rest")
+            end,
+            { nargs = 0 }
+        )
     end
 }
